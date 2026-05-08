2dc8b0ec007f70934c1566b6f1f8c30736c38803
ruxuangan <ruxuangan@gmail.com>
2026-05-08 11:41:04 +0800

    5.8日课堂内容

--- 2026.5.8/06.py
+++ 2026.5.8/06.py
@@ -1,1 +1,45 @@
-my_file = open(file='data/sample.txt', mode='w')
+import matplotlib.pyplot as plt
+
+# x = ["A", "B", "C"]
+# y = [5, 15, 12]
+# fig,ax = plt.subplots(figsize=(2,2))
+# ax.plot(x,y, linewidth = 2, color = "black", linestyle = ":");   #plot is a line plot  
+# ax.scatter(x, y);
+
+# fig,ax = plt.subplots(nrows = 1 , ncols = 2 , figsize = (5,2))
+# ax[0].plot(x, y);
+# ax[1].bar(x,y);
+
+# fig, ax = plt.subplots(nrows = 3 , ncols = 2 , figsize = (3,5))
+# ax[0][1].scatter(x,y)
+
+# plt.show()
+
+students = {"Bob" : 18 ,"Alice" : 12 , "John" : 15}
+x = students.keys()
+y = students.values()
+avg = sum(y)/ len(y)
+
+fig, ax = plt.subplots( figsize = (2,2 ))
+ax.bar(x,y);
+ax.set_xlabel("names");
+ax.set_ylabel("grades");
+ax.set_title("Python Class");
+ax.axhline( avg , color = "green");
+ax.text(3, avg, f"average:{avg}", color = "green");
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

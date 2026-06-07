6b710bf85b1cf658ab5c9f53bda2195b222af5f7
ruxuangan <ruxuangan@gmail.com>
2026-06-07 23:56:27 +0800

    complete all notes

--- 2026.5.11/07.py
+++ 2026.5.11/07.py
@@ -20,75 +20,76 @@ cols_to_rename = {'floorAreaSqM' : 'sqm' ,
                   'currentEnergyRating' : 'energy',
                   'history_price' : 'price'}
 df = df.rename(columns = cols_to_rename)
-print(df.head(3))
+# print(df.head(3))
 
 # check for missing values
-print(df.isnull().sum()) 
+# print(df.isnull().sum()) 
 
 # missing values in numerical cols
-sqm_avg = df['sqm'].mean()
-df['sqm'].fillna( sqm_avg )
+# sqm_avg = df['sqm'].mean()
+# df['sqm'].fillna( sqm_avg )
 
 # missing values in categorical cols
-energy_v = df['energy'].mode() #most frequent value
-df['energy'].fillna( energy_v )
+# energy_v = df['energy'].mode() #most frequent value
+# df['energy'].fillna( energy_v )
 
 # # there are some advanced missing values imputation technology
 # # if you fill missing values , you create fake data
 
 # # simple approach : remove all the rows with missing values
-# df = df.dropna()
+df = df.dropna()
 # print(df.shape)
 # df.head(3)
 # df.isnull().sum()
 
 # # type conversion in pandas
 # df['bathrooms'] = df['bathrooms'].astype(int)
-# df.dtypes
-# df.head(1)
+# print(df.dtypes)
+# print(df.head(1))
 
-# # convert numerical col to categorical
+# convert numerical col to categorical
 # df['price_cat_1'] = pd.cut( df['price'], bins = 3 , labels = ['Low', 'Mid', 'High'])
-# df['price_cat_1'].value_counts()
-# df['price'].min(), df['price'].max()
+# print(df['price_cat_1'].value_counts())
+# print(df['price'].min(), df['price'].max())
 
-# def my_price_map( x ):
-#     if x < 100000 :
-#         return "Low"
-#     elif x < 500000 :
-#         return 'Mid'
-#     else :
-#         return 'High'
-# # apply a function on every value in a col
-# df['price_cat_2'] = df['price'].apply( my_price_map )
+def my_price_map( x ):
+    if x < 100000 :
+        return "Low"
+    elif x < 500000 :
+        return 'Mid'
+    else :
+        return 'High'
+# apply a function on every value in a col
+df['price_cat_2'] = df['price'].apply( my_price_map )
 # df['price_cat_2'].value_counts()
 # def my_energy_map(x):
 #     if x in ['A', 'B', 'C'] :
 #         return 'efficient'
 #     else :
 #         return 'not efficient'
-# df['energy'].apply( my_energy_map)
-# df['energy'].apply( lambda x : 'efficient' if x in ['A', 'B', "C"] else 'not efficient')
+# df['energy'] = df['energy'].apply( my_energy_map)
+# df['energy'] = df['energy'].apply( lambda x : 'efficient' if x in ['A', 'B', "C"] else 'not efficient')
 # print(df.head(3))    
 
 
 # # Data exploration
 # # univariate analysis : one feature (column) at a time
 # # bivariate analysis
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# mask = df['price_cat_2'] == "Low"
-# df_low = df[mask]
+import matplotlib.pyplot as plt
+import seaborn as sns
+mask = df['price_cat_2'] == "Low"
+df_low = df[mask]
+print(df_low)
 # df_low.shape
 
-# # numerical cols
-# # panda style
+# numerical cols
+# panda style
 # df_low["price"].plot( kind = 'box');
 # df_low['price'].plot( kind = 'hist');
 
-# # seaborn style
-# # sns.boxplot(data = df_low , x = 'price);
-# # sns.histplot( data = df_low , x = 'price');
+# seaborn style
+# sns.boxplot(data = df_low , x = 'price');
+# sns.histplot( data = df_low , x = 'price');
 # sns.displot(data = df_low, x = 'price',kde = True);
 
 # # categorical cols
@@ -96,30 +97,31 @@ df['energy'].fillna( energy_v )
 # df['energy'].value_counts().plot( kind = 'bar');
 
 # # seaborn style
-# # sns.countplot( df['energy']) ;
+# sns.countplot( df['energy']) ;
 # sns.barplot( df['energy'].value_counts());
 
-# # bivariate plots
-# # pandas style
-# # df_low.plot( kind = 'scatter', x = 'sqm' ,y = 'price');
+# bivariate plots
+# pandas style
+# df_low.plot( kind = 'scatter', x = 'sqm' ,y = 'price');
 
-# # seaborn style
-# # sns.scatterplot( data = df_low , x = 'sqm' , y = 'price');
+# seaborn style
+# sns.scatterplot( data = df_low , x = 'sqm' , y = 'price');
 
-# #price dist.per energy level
+#price dist. per energy level
 # sns.boxplot(data = df_low , x = 'energy' , y = 'price');
 
 # # price dist. per energy level and per tenure
 # plt.figure(figsize = (10,3));
 # sns.boxplot( data = df_low , x = 'energy' , y = 'price', hue = 'tenure');
 
-# # Note : select numerical cols
-# df_num = df_low.select_dtypes( include = 'number')
-# df_num.head(3)
+# Note : select numerical cols
+df_num = df_low.select_dtypes( include = 'number')
+# print(df_num.head(3))
 
 # # Correlation plot
-# plt.figure( figsize = (4,3));
-# sns.heatmap(df_num.corr(), annot = True ,fmt = '.1f' ,cmap = 'Blues')
-# # annot : see the values
-# # fmt : format of the values : 1 floating point
-# # cmap : color map ( you can select from a range of color map)
+plt.figure( figsize = (4,3));
+sns.heatmap(df_num.corr(), annot = True ,fmt = '.1f' ,cmap = 'Blues')
+# annot : see the values
+# fmt : format of the values : 1 floating point
+# cmap : color map ( you can select from a range of color map)
+plt.show()
--- 2026.5.8/06.py
+++ 2026.5.8/06.py
@@ -1,19 +1,19 @@
 import matplotlib.pyplot as plt
 
-x = ["A", "B", "C"]
-y = [5, 15, 12]
-fig,ax = plt.subplots(figsize=(2,2))
-ax.plot(x,y, linewidth = 2, color = "black", linestyle = ":");   #plot is a line plot  
-ax.scatter(x, y);
+# x = ["A", "B", "C"]
+# y = [5, 15, 12]
+# fig,ax = plt.subplots(figsize=(2,2))
+# ax.plot(x,y, linewidth = 2, color = "black", linestyle = ":");   #plot is a line plot  
+# ax.scatter(x, y);
 
-fig,ax = plt.subplots(nrows = 1 , ncols = 2 , figsize = (5,2))
-ax[0].plot(x, y);
-ax[1].bar(x,y);
+# fig,ax = plt.subplots(nrows = 1 , ncols = 2 , figsize = (5,2))
+# ax[0].plot(x, y);
+# ax[1].bar(x,y);
 
-fig, ax = plt.subplots(nrows = 3 , ncols = 2 , figsize = (3,5))
-ax[0][1].scatter(x,y)
+# fig, ax = plt.subplots(nrows = 3 , ncols = 2 , figsize = (3,5))
+# ax[0][1].scatter(x,y)
 
-plt.show()
+# plt.show()
 
 # students = {"Bob" : 18 ,"Alice" : 12 , "John" : 15}
 # x = students.keys()
@@ -38,13 +38,13 @@ my_data = {
   }
 
 df = pd.DataFrame( data = my_data, index = ["a", "b", "c", "d"])
-# print(df)
+print(df)
 
 # print( df.shape)
 # print( df.columns)
 # print( df.index)
 # print(df.info())
-# df.describe()  # get stats info on numerical cols
+# print(df.describe())  # get stats info on numerical cols
 # df.describe(include = "o")  # get stats info on object cols
 
 # #access col /cols
@@ -77,22 +77,21 @@ df = pd.DataFrame( data = my_data, index = ["a", "b", "c", "d"])
 
 # # add a col
 df.insert(2, "Sex", ["Male", "Female", "Male", "Female"])
-# print(df)
+print(df)
 # #remove a col ( two way )
-# df.drop(columns= ["Sex"], inplace = True)
-# print(df)
+df.drop(columns= ["Sex"], inplace = True)
+print(df)
 # df = df.drop(columns= ["Sex"])
 # print(df)
 
 # level 1 aggregation -- what is the average of age
-print(df["Age"].mean()) # size, max, count, size
-# level 2 aggregation -- what is the average of age in each city
-print(df.groupby("City")["Age"].mean())
-# level 3 aggregation -- the average of age per city and per sex
-print(df.pivot_table(index = "City", columns = "Sex", values = "Age", aggfunc = "mean"))
-# index : first group
-# columns :second group
-
+# print(df["Age"].mean()) # size, max, count, size
+# # level 2 aggregation -- what is the average of age in each city
+# print(df.groupby("City")["Age"].mean())
+# # level 3 aggregation -- the average of age per city and per sex
+# print(df.pivot_table(index = "City", columns = "Sex", values = "Age", aggfunc = "mean"))
+# # index : first group
+# # columns :second group
 
 
 
